a2eec8b5b12d878227830536b81d2d70aa594d75
palloncino <antonio.guiotto.dev@gmail.com>
2024-05-13 20:18:28 +0200

    progress

---
+++ app/subs/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Subs = () => {
+  return (
+    <div>
+      <h1>Subscriptions</h1>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Subs;
